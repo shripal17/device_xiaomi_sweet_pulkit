a7577abf495199fae7f35110d66b7c4b7185c5ac
Shripal Jain <shripal17@gmail.com>
2023-07-19 21:28:08 +0200

    sweet: Blissify
    Signed-off-by: Shripal Jain <shripal17@gmail.com>

--- bliss_sweet.mk
+++ bliss_sweet.mk
@@ -11,10 +11,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Bliss stuff
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
+# Bliss build type
+BLISS_BUILDTYPE := OFFICIAL
+
+# Device related
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := bliss_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
