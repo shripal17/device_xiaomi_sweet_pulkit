e78c65e929e40b52574f87553a3e6876e6475009
LuK1337 <priv.luk@gmail.com>
2024-05-20 12:20:46 +0530

    sweet: Redo libsdmcore shim Instead of injecting custom dependency into libsdmcore.so, provide a custom hwcomposer implementation that overrides the *unwanted* symbol before loading the vendor implementation.
    Signed-off-by: Alen 24 <alenbijuk2003@gmail.com>

--- device.mk
+++ device.mk
@@ -127,7 +127,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml
 
 PRODUCT_PACKAGES += \
-    libsdmcore-shim
+    hwcomposer.qcom
 
 # Device-specific settings
 PRODUCT_PACKAGES += \
